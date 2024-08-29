83829955f89e14aad190ebd044680bf76e739e71
Hleb <gleb.qb@gmail.com>
2024-08-29 17:55:23 +0200

    Initial commit: added backend and frontend projects

---
+++ backend/shop-engine/src/test/java/ru/bublinoid/shopengine/ShopEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bublinoid.shopengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
